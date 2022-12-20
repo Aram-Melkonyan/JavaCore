d15ee3c5e2c88e884d75383fb2a53ea3f807e878
aram mwlkonyan <arammelk77@gmail.com>
2022-12-20 16:25:36 +0400

    add homework 1

--- src/homework1/FigurePainter.java
+++ src/homework1/FigurePainter.java
@@ -2,13 +2,15 @@ package homework1;
 
 public class FigurePainter {
     public static void main(String[] args) {
-
+        System.out.println("Figure 1");
         for (int i = 0; i < 7; i++) {
             for (int j = 0; j < i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
+        System.out.println();
+        System.out.println("Figure 2");
         for (int i = 0; i < 7; i++) {
             for (int j = 7; j > i; j--) {
                 System.out.print("  ");
@@ -19,6 +21,7 @@ public class FigurePainter {
             }
             System.out.println();
         }
+        System.out.println("Figure 3");
         System.out.println();
         for (int i = 0; i < 7; i++) {
             for (int j = 7; j > i; j--) {
