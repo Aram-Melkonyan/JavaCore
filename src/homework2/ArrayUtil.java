ab3e06a8502b6e7b069065cd0156598a8cccccf5
aram mwlkonyan <arammelk77@gmail.com>
2022-12-23 19:05:50 +0400

    add homework 2

--- src/homework2/ArrayUtil.java
+++ src/homework2/ArrayUtil.java
@@ -4,18 +4,18 @@ import java.util.Arrays;
 
 public class ArrayUtil {
     public static void main(String[] args) {
-        int[] nubers = {1, 3, 5, 7, 8, 9, 11, 14, 6, 10};
-        System.out.println(nubers.length);
-        for (int i = 0; i < nubers.length; i++) {
-            System.out.print(nubers[i] + ",");
+        int[] numbers = {1, 3, 5, 7, 8, 9, 11, 14, 6, 10};
+        System.out.println(numbers.length);
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + ",");
         }
         System.out.println();
         for (int i = 0; i < 10; i++) {
-            if (nubers[i] == 14) {
-                System.out.println("max " + nubers[i]);
+            if (numbers[i] == 14) {
+                System.out.println("max " + numbers[i]);
             }
-            if (nubers[i] == 1) {
-                System.out.println("min " + nubers[i]);
+            if (numbers[i] == 1) {
+                System.out.println("min " + numbers[i]);
             }
         }
     }
