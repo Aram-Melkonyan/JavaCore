ad0347ee84881b550690a434d8f96dc18cb7d4ad
aram mwlkonyan <arammelk77@gmail.com>
2023-01-08 00:52:05 +0400

    add homework 4 change

--- src/homework1/homework4/CharArrayExample.java
+++ src/homework1/homework4/CharArrayExample.java
@@ -12,12 +12,20 @@ public class CharArrayExample {
         }
         System.out.println("c-փոփոխականից կա " + a);
         char[] chars3 = {'j', 'a', 'v', 'a', 'l', 'o', 'l', 'y'};
-        if (chars3[chars3.length - 1] == 'y' && chars3[chars3.length - 2] == 'l') ;{
+        if (chars3[chars3.length - 1] == 'y' && chars3[chars3.length - 2] == 'l') ;
+        {
             System.out.println(true);
         }
         char[] text = {' ', ' ', 'b', 'a', 'r', 'e', 'v', ' ', ' '};
         for (int i = 2; i < text.length - 2; i++) {
             System.out.print(text[i]);
         }
+        System.out.println();
+        char[] chars2 = {'j', 'a', 'v', 'a', 'l', 'o', 'v', 'e'};
+        int b = 0;
+        int d = 0;
+        b = chars2.length / 2;
+        d = chars2.length / 2 - 1;
+        System.out.println(b + " " + d);
     }
 }
