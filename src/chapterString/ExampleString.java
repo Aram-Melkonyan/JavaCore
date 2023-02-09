48b420a3a949a05efed83d25ca4f40a8bf17dd14
aram mwlkonyan <arammelk77@gmail.com>
2023-02-10 00:33:28 +0400

    and homework employee change

---
+++ src/chapterString/ExampleString.java
@@ -0,0 +1,14 @@
+package chapterString;
+
+public class ExampleString {
+    public static void main(String[] args) {
+
+
+     String name = " Aram Melkonyan   ";
+        System.out.println(name);
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.trim());
+
+    }
+}
